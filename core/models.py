ef53748b39c074373b354ca9f7edcb03922b07e8
Yao Josué <46876900+MJoshua25@users.noreply.github.com>
2023-01-31 07:00:15 +0000

    adding the standard model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Standard_model(models.Model):
+	statut = models.BooleanField(default=True)
+	date_add = models.DateTimeField(auto_now_add=True)
+	date_upd = models.DateTimeField(auto_now=True)
+
+	class Meta:
+		abstract = True
